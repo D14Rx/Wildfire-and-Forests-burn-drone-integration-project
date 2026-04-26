7818aacede0e58b660cae8213fcc40b9cdac8191
D14Rx <diasklzhnv@gmail.com>
2026-04-26 20:48:42 +0500

    Update psql_forestwatch.py

--- psql_forestwatch.py
+++ psql_forestwatch.py
@@ -1,8 +1,7 @@
-import psycopg2
-from psycopg2.extras import DictCursor
+import asyncpg
 
-async def get_connect():
-    return psycopg2.connect(
+async def post_data(tree_s, fire_s, fire_c):
+    conn = await asyncpg.connect(
         database = "test_db", 
         user = "postgres",
         password = "diar2011",
@@ -10,22 +9,17 @@ async def get_connect():
         port = 8000
     )
 
-async def post_data(tree_s, fire_s, fire_c, time):
-    query = "INSERT INTO forest_d(tree_spoted, fire_spotted, fire_chance, detected_at)VALUES(%s, %s, %s, CURRENT_TIMESTAMP)"
-    with get_connect() as conn:
-        with conn.cursor() as cursor:
-            cursor.execute(query, (tree_s, fire_s, fire_c, time))
-            conn.comit()
+    query = """INSERT INTO forest_d(tree_spoted, fire_spotted, fire_chance, detected_at)
+                VALUES($1, $2, $3, CURRENT_TIMESTAMP)"""
+    
+    await conn.execute(query, tree_s, fire_s, fire_c)
+    await conn.close()
 
 async def show_all():
-    with get_connect() as conn:
-        with conn.cursor(cursor_factory=DictCursor) as cursor:
-            cursor.execute("SELECT * FROM forest_d")
-            return cursor.fetchall() 
-
-async def update_forest_status(tree_id, status: bool):
-    query = "UPDATE forest_d SET tree_s =%s WHERE id =%s"
-    with get_connect() as conn:
-        with conn.cursor() as cursor:
-            cursor.execute(query, (status, tree_id))
-            conn.comit()
+    conn = await asyncpg.connect(
+        database="test_db", user="postgres", password="diar2011", host="127.0.0.1", port=8000
+    )
+    #rows - A horizonal interaction in PSQL tables like with lists function 
+    rows = await conn.fetch("SELECT * FROM forest_d") #fetch() returning data in dict looking format
+    await conn.close()
+    return rows
